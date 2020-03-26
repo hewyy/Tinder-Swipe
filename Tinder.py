f04f3a62bb9c67093206b1af3ea4f347fddb0f00
Hew Boland <55921408+hewyy@users.noreply.github.com>
2020-03-25 22:56:30 -0400

    Create Tinder.py

---
+++ Tinder.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import os
+
+
+options = Options()
+#options.add_argument("--headless")
+options.add_argument("--window-size=1920x1080")
+
+filename = "C:\\Users\\hewbo\\AppData\\Local\\Programs\\Python\\chromedriver.exe"
+
+
+
+browser = webdriver.Chrome(options=options, executable_path=filename)
+
+browser.get("https://tinder.com/app/recs")
+
+
+
+time.sleep(50)
+
+
+like = browser.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]")
+
+
+
+while(True):
+	like.click()
+
